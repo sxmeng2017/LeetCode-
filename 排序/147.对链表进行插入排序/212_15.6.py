0e8679b94939a2eba96920637051a24fdb837695
sxmeng2017 <570701164@qq.com>
2019-10-16 23:58:02 +0800

    147
    147

---
+++ 排序/147.对链表进行插入排序/212_15.6.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            if head.val <= head.next.val:
+                head = head.next
+                continue
+            pre = dummy
+            while pre.next.val < head.next.val:
+                pre = pre.next
+            cur = head.next
+            head.next = cur.next
+            cur.next = pre.next
+            pre.next = cur
+        return dummy.next
+
